464a5a04bc4dbe7d72bdbcf1d36c3cfe334680a2
kevy <kv_vinu@hotmail.com>
2019-11-27 15:22:35 +0530

    Added BG color

--- html/one/color.js
+++ html/one/color.js
@@ -1,19 +1,12 @@
 const now = document.getElementById("now");
-const upcoming = document.getElementById("upcoming");
 function highlight() {
   const words = now.textContent.split(" ");
   now.innerHTML = "";
   words.forEach((word) => {
     const span = now.appendChild(document.createElement('span'));
     span.textContent = word + ' ';
-    if (word === 'Vacant') span.classList.add('green');
-  const words = upcoming.textContent.split(" ");
-  upcoming.innerHTML = "";
-  words.forEach((word) => {
-    const span = upcoming.appendChild(document.createElement('span'));
-    span.textContent = word + ' ';
-    if (word === 'Vacant') span.classList.add('green');
-});
+    if (word === 'Vacant') document.body.style.backgroundColor ="palegreen";
+  });
 };
-color.addEventListener("blur", highlight);
+now.addEventListener("blur", highlight);
 highlight();
